e15d410427087ada968190e7ce611e343f0dee57
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-01-13 20:17:09 +0900

    feat: 리뷰 저장을 위한 pullrequest 모델 생성

--- pullrequest/models.py
+++ pullrequest/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class PRReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100) #####
+    pr_url = models.URLField(max_length=255)
+    aver_grade = models.CharField(max_length=20)
+    problem_type = models.CharField(max_length=20, null=True, blank=True)
+    review_mode = models.CharField(max_length=20)
+    al_review = models.TextField()
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class FileReview(models.Model):
+    id = models.AutoField(primary_key=True)
+    pr_review = models.ForeignKey(PRReview, on_delete=models.CASCADE)
+    file_path = models.CharField(max_length=255)
+    comment = models.TextField()
+    grade = models.CharField(max_length=20)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)  # 기본값 제거
+    updated_at = models.DateTimeField(auto_now=True)
