7e785d3dd35143373f51db9cf9f997c6a9301728
einhn <ainkm@naver.com>
2025-01-13 20:55:26 +0900

    feat: PR리뷰 검색 기능 구현(페이지네이션 적용 전)

--- pullrequest/models.py
+++ pullrequest/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 
 class PRReview(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100) #####
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, default="Untitled")
     pr_url = models.URLField(max_length=255)
     aver_grade = models.CharField(max_length=20)
     problem_type = models.CharField(max_length=20, null=True, blank=True)
