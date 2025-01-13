2d7ee91afec497827b0b5946b1eb5bea606d8f46
einhn <ainkm@naver.com>
2025-01-13 20:20:25 +0900

    feat: 리뷰 검색 기능 구현(페이지네이션 등 적용 전)

--- pullrequest/models.py
+++ pullrequest/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PRReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100) #####
+    pr_url = models.URLField(max_length=255)
+    aver_grade = models.CharField(max_length=20)
+    problem_type = models.CharField(max_length=20, null=True, blank=True)
+    review_mode = models.CharField(max_length=20)
+    total_review = models.TextField()
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
