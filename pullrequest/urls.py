7681aeab708f511230d9901282c494ba2261908d
einhn <ainkm@naver.com>
2025-01-14 15:44:26 +0900

    feat: 최신 7개 PR의 평균 등급 조회 기능 구현

--- pullrequest/urls.py
+++ pullrequest/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PRReviewListView, PRReviewSearchView
+from .views import PRReviewListView, PRReviewSearchView, PRReviewAverageGradeView
 
 urlpatterns = [
     path('v1/pr-reviews/', PRReviewListView.as_view(), name='pr-review-list'),
     path('v1/pr-reviews/search', PRReviewSearchView.as_view(), name='pr-review-search'),
+    path('v1/pr-reviews/aver-grade', PRReviewAverageGradeView.as_view(), name='pr-review-aver-grade'),
 ]
 
