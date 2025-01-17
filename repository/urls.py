fd8b78f5f8280ba7413f655eaadb79d4a881c610
RYUJEONGHUN <ryugh0210@naver.com>
2025-01-17 21:51:26 +0900

    Revert "[feat/#10] 마지막 기능들 모두구현"

--- repository/urls.py
+++ repository/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
 from . import views
-from .views import get_repositories, get_acitve, get_inacitve, get_search
-
+from .views import get_repositories
 from .views import ApplyRepositoryView
 urlpatterns = [
     path('repositories/', get_repositories, name='get_repositories'),
     path('repositories/select/', ApplyRepositoryView.as_view(), name='ApplyRepositoryView'),
-
-    path('repositories/active/',get_acitve, name='get_acitve'),
-
-    path('repositories/inactive/',get_inacitve, name='get_inactive'),
-
-    path('repositories/search/',get_search, name='get_search'),
 ]
+
