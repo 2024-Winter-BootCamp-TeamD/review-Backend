9379fa19682da725c5b96f110b8cc35f6721a6ca
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-01-26 15:52:59 +0900

    fix: url추가

--- apiserver/urls.py
+++ apiserver/urls.py
@@ -41,6 +41,7 @@ urlpatterns = [
     path('api/', include('partreview.urls')), # 드래그 코드 엔드포인트
     path('api/', include('report.urls')), # 보고서 엔드포인트
     path('api/v1/', include('repository.urls')),  # 레포지토리 엔드포인트
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger')
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger'),
+    path('', include('django_prometheus.urls')),
 ]
 
