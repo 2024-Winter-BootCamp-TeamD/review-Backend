1f9a39cf087276f9e2b81e089a989e758e2845ee
fostacion <rever0577@gmail.com>
2025-01-27 00:20:56 +0900

    prreview 정보 확인 수정 및 프롬프트 개선 추가 수정

--- pullrequest/urls.py
+++ pullrequest/urls.py
@@ -4,6 +4,8 @@ from .views import PRReviewListView, PRReviewSearchView, PRReviewTroubleTypeView
 
 urlpatterns = [
     path('v1/pr-reviews/', PRReviewListView.as_view(), name='pr-review-list'),
+    path('v1/pr-reviews/search', PRReviewSearchView.as_view(), name='pr-review-search'),
+    path('v1/pr-reviews/aver-grade/recent', PRReviewRecentAverageGradeView.as_view(), name='pr-review-aver-grade-recent'),
     path('v1/pr-reviews/aver-grade/all', PRReviewAllAverageGradeView.as_view(), name='pr-review-aver-grade-all'),
     path('v1/pr-reviews/trouble-type', PRReviewTroubleTypeView.as_view(), name='pr-review-trouble-type'),
     path('v1/pr-reviews/category', PRReviewCategoryStatisticsView().as_view(), name='pr-review-category'),
